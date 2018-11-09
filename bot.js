b75862bb326d17cd1c7cf6492635ea41fd50018e
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-09 15:51:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,6 @@ client.on('message',function(message) {
 let args = message.content.split(" ").slice(1).join(" ");
 if(message.content.startsWith(prefix + "say")) {
   if (message.author.id !== '507138559640076294') return message.nothing
-  if(!message.author.id === '507138559640076294') return;
 if('') return;
 message.channel.send(`${args}`);
 }
