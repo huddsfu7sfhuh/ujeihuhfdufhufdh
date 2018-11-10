b8a9df4d28d274b996b95ebf12dbcaf07220c3cd
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-10 16:51:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,4 +24,19 @@ message.channel.send(`${args}`);
 }
 });
 
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("493590861461061652").join(); //by : D7i
+    });
+
 client.login(process.env.BOT_TOKEN); 
