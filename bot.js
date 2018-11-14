0933b28f999e95624dd4e752cf1c89f21161fc09
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-14 18:17:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,39 +24,4 @@ message.channel.send(`${args}`);
 }
 });
 
-client.on('ready',async () => {
-  let GUILDID = '512282617329221632';
-  let CHANNELID = '493590861461061652'; 
-  voiceStay(GUILDID, CHANNELID);
-  function voiceStay(guildid, channelid) {
-    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
-    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
-
-    let guild = client.guilds.get(guildid);
-    let channel = guild.channels.get(channelid);
-
-    if(channel.type === 'voice') {
-      channel.join().catch(e => {
-        console.log(`Failed To Join :: ${e.message}`);
-      });
-    } else {
-      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
-    }
-  }
-});
-
-client.on('ready',async () => {
-console.log("Starting..");
-let g = client.guilds.get("492851193010192424");
-let c = g.channels.get("512284275870138378");
-if(c.type === 'voice') {
-c.join();
-setInterval(() => {
-if(!g.me.voiceChannel) c.join();
-}, 1);
-} else {
-console.log("Failed To Join:\n The Channel Type isn't \"text\"");
-}
-});
-
 client.login(process.env.BOT_TOKEN); 
